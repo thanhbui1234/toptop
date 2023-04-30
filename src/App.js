c5a62fd825fd4138ee001157cca1b8d79a776a96
thanh <miktenthanh@gmail.com>
2023-04-30 23:50:51 +0700

    fist commit with react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+const gifts = ["vu to", "lon"];
 
 function App() {
+  const [gift, setGift] = useState();
+
+  const handleSetGift = () => {
+    const index = Math.floor(Math.random() * gifts.length);
+
+    setGift(gifts[index]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{gift || "Nhan vao day de nhan phan thuong"}</h1>
+      <button onClick={handleSetGift}>Click</button>
     </div>
   );
 }
